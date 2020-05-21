915fbaf87a0eee0ecc3779f7ddaf2217a31db27a
kksanderr <manga978@gmail.com>
2020-05-21 19:29:18 +0200

    subscribe i footer sekcija

--- index.php
+++ index.php
@@ -129,6 +129,47 @@
         <div class="blog-btn">
           <a href="#">See More</a>
         </div>
+    </section> <!-- end blog -->
+
+    <!-- Subscribe -->
+    <section class="subscribe" id="subscribe">
+      <div class="container">
+        <div class="subscribe-content">
+          <h2>Subscribe Here for Updates</h2>
+          <form action="subscribe.php" method="post">
+            <input class="subscribe-field" type="text" name="email" placeholder="Email">
+            <input class="subscribe-button" type="submit" value="Subscribe">
+          </form>
+        </div>
+      </div>
     </section>
+    <!-- End Subscribe -->
+
+    <!-- Footer -->
+    <footer class="footer-wrapper">
+      <div class="container">
+        <div class="footer">
+          <ul class="nav-list">
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li class="footer-list-divider">|</li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li class="footer-list-divider">|</li>
+            <li>
+              <a href="login.html">Login</a>
+            </li>
+            <li class="footer-list-divider">|</li>
+            <li>
+              <a href="signup.html">Signup</a>
+            </li>
+            <p class="copyright mt-2">Copyright &copy; Limischool <?php echo date('Y') ?></p>
+          </ul>
+        </div>
+      </div>
+    </footer>
+    <!-- End Footer -->
   </body>
 </html>
