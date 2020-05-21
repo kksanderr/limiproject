1ba3e28b431117a0917f49caf5c27a163bfb338e
kksanderr <manga978@gmail.com>
2020-05-21 20:00:33 +0200

    dodat dropdown u navbar

--- templates/header.php
+++ templates/header.php
@@ -18,6 +18,31 @@
             <li>
               <a href="signup.php">Signup</a>
             </li>
+
+            <!-- Dropdown -->
+            <li class="dropdown-wrapper">
+              <a href="#"> Dropdown </a>
+              <ul class="dropdown">
+                <li>
+                  <a href="#">
+                    Drop 1
+                  </a>
+                </li>
+
+                <li>
+                  <a href="#">
+                    Drop 2
+                  </a>
+                </li>
+
+                <li>
+                  <a href="#">
+                    Drop 3
+                  </a>
+                </li>
+              </ul>
+          </li>
+          <!-- end dropdown -->
           </ul>
       </nav>
     </div>
